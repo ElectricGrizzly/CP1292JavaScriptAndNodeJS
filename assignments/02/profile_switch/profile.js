65500ee180f76c964e30e53f79f5287c471c917c
Jeffrey D. Daye, B.Eng <jeffreydaye@protonmail.com>
2022-01-27 16:52:25 -0330

    Added leap year functionality (for fun!)

--- assignments/02/profile_switch/profile.js
+++ assignments/02/profile_switch/profile.js
@@ -14,12 +14,29 @@ const isDate = text => {
 	const index2 = text.indexOf( "/", index1 + 1 );
 	const month = parseInt( text.substring( 0, index1 ) );
 	const day = parseInt( text.substring( index1 + 1, index2 ) );
+    // year is used for the unasked for leap year check (for fun)
+    const year = parseInt( text.substring( index2 + 1, text.length ) );
+    console.log(year);
 	
 	if( month < 1 || month > 12 ) { return false; }
 	else {
         switch(month) {
             case 2:
-                if(day > 28) {return false;}
+                if(day > 28) {
+                    // criterion for Julian leap year is year % 4 == 0
+                    // for Gregorian, the year must be a Julian leap year
+                    // that does not divide by 100 evenly,
+                    // unless it is also evenly divisible by 400. 
+                    // NOT NEEDED FOR ASSIGNMENT 2
+                    if (year % 4 == 0){
+                        if (year % 100 == 0) {
+                            if (year % 400 == 0) {break;}
+                            else {return false;}
+                        }
+                        break;
+                    }
+                    return false;
+                }
                 break;
             case 4:
             case 6:
